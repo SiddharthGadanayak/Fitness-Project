289be4390b603e4a33f0ed3ef4ed43e6d7b998dd
Gurpreet Kaur <155377816+gurpreetk3012@users.noreply.github.com>
2025-10-18 11:47:41 +0530

    Updated the dynamic fetch

--- backend/app/models.py
+++ backend/app/models.py
@@ -5,3 +5,7 @@ class User(db.Model):
     name = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    age = db.Column(db.Integer)
+    height = db.Column(db.Float)
+    weight = db.Column(db.Float)
+    gender = db.Column(db.String(10))
